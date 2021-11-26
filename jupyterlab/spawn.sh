f8d8a442acd0e5ff9fd5d0880a6db7f4db37f80a
MircoT <MircoT@users.noreply.github.com>
2021-11-26 11:35:38 +0100

    Fix dask extension versions (#6)
    * Fix dask plugins
    
    * Update jupyterlab image action
    
    * Add hatcondor dask wn image
    
    * Fix dask versions
    
    * Update dask image actions
    
    * Move htcondor dask wn image
    
    * Update htcondor dask wn image
    
    * Fix python package versions
    
    * Update dockefile
    
    * Fix oidc-agent installation
    
    * Update remote jobqueue plugin
    
    * Update dask-labextension
    
    * Move rucio extension
    
    * Add tini to executer
    
    * Add tini and fix minor warnings
    
    * Update dask libs
    
    * Fix scripts
    
    * Change actions
    
    * Fix action names

--- jupyterlab/spawn.sh
+++ jupyterlab/spawn.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Configure oidc-agent for user token management
-echo "eval \`oidc-keychain\`" >> ~/.bashrc
-eval `oidc-keychain`
+echo "eval \`oidc-keychain\`" >>~/.bashrc
+eval $(oidc-keychain)
 oidc-gen dodas --issuer $IAM_SERVER \
-               --client-id $IAM_CLIENT_ID \
-               --client-secret $IAM_CLIENT_SECRET \
-               --rt $REFRESH_TOKEN \
-               --confirm-yes \
-               --scope "openid profile email wlcg wlcg.groups" \
-               --redirect-uri  http://localhost:8843 \
-               --pw-cmd "echo \"DUMMY PWD\""
+    --client-id $IAM_CLIENT_ID \
+    --client-secret $IAM_CLIENT_SECRET \
+    --rt $REFRESH_TOKEN \
+    --confirm-yes \
+    --scope "openid profile email wlcg wlcg.groups" \
+    --redirect-uri http://localhost:8843 \
+    --pw-cmd "echo \"DUMMY PWD\""
 
-while true; do oidc-token dodas --time 1200 > /tmp/token; sleep 600; done &
+while true; do
+    oidc-token dodas --time 1200 >/tmp/token
+    sleep 600
+done &
 
 if [[ -f "/cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh" ]]; then
     source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
