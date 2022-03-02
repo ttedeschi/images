0f94a4c2bafc75d14dc88ff908fdfca41211fe29
MircoT <MircoT@users.noreply.github.com>
2022-03-02 09:42:24 +0100

    Change thisroot script

--- jupyterlab/spawn.sh
+++ jupyterlab/spawn.sh
@@ -26,7 +26,7 @@ done &
 if [[ -f "/cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh" ]]; then
     source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
     conda activate cms-dodas
-    source /cvmfs/cms.dodas.infn.it/miniconda3/envs/cms-dodas/bin/thisroot.sh
+    source /cvmfs/cms.dodas.infn.it/root-ubuntu/bin/thisroot.sh
     #export LD_LIBRARY_PATH=/cvmfs/cms.dodas.infn.it/miniconda3/envs/cms-dodas/lib:$LD_LIBRARY_PATH
     export JUPYTER_PATH=/opt/conda/etc/jupyter
     export JUPYTER_CONFIG_DIR=/opt/conda/etc/jupyter
